795249de01290de686ad80542fe8db556291e1d4
Tal <taldahan3@gmail.com>
2018-10-03 23:34:43 +0300

    Rename and unneeded comments delete

--- CatClassifierV2.py
+++ CatClassifierV2.py
@@ -71,24 +71,8 @@ def getImageLocal(path):
         img = Image.open(path)
     except (Exception):
         return None  
-
-    #imgnew = img.resize((224,224),Image.ANTIALIAS)
-    # convert the PIL image to a numpy array
-    # IN PIL - image is in (width, height, channel)
-    # In Numpy - image is in (height, width, channel)
     numpy_image = img_to_array(img) 
-    # Convert the image / images into batch format
-    # expand_dims will add an extra dimension to the data at a particular axis
-    # We want the input matrix to the network to be of the form (batchsize, height, width, channels)
-    # Thus we add the extra dimension to the axis 0.
     image_batch = np.expand_dims(numpy_image, axis=0)
-    #print('image batch size', image_batch.shape)
-    #if image_batch.shape[3] != 3:
-   #     return None
-    #try:
-    #    imgnew.save(r'Images/'+url[1]+'_'+sn+r'.jpg')
-    #except (Exception):
-    #    pass #dummy
     return image_batch
 
 def getCategoryFromTalNet(talCat):
